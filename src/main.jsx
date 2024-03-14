5d6f1f188ebef0db6d5f82a5cba4d4e2e4a6b90a
Aleksandr-Reznichenko <reznichenko.aleksandr.v@gmail.com>
2024-03-14 15:22:42 +0200

    Installing and configuring Redux and React Redux libraries
    A library is also installed that allows you to initialize the Redux DevTools logic and link it to an extension in the developer tools.

--- src/main.jsx
+++ src/main.jsx
@@ -5,12 +5,16 @@ import { BrowserRouter } from 'react-router-dom';
 // Import modern-normalize
 import 'modern-normalize';
 import App from './components/App/App.jsx';
+import { Provider } from 'react-redux';
+import { store } from './redux/store.js';
 import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <BrowserRouter>
-      <App />
+      <Provider store={store}>
+        <App />
+      </Provider>
     </BrowserRouter>
   </React.StrictMode>
 );
